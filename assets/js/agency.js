5128331fb3b7827106c590dee71665805606356a
UtkarshMishra04 <33784374+UtkarshMishra04@users.noreply.github.com>
2018-10-23 06:00:11 +0530

    minor update

--- assets/js/agency.js
+++ assets/js/agency.js
@@ -30,4 +30,50 @@
         }
     })
 
-})(jQuery); // End of use strict
+  $("#testitoggle1").click(function() {
+    var elem = $("#testitoggle1").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#testitoggle1").text("Read Less");
+      $("#testitext1").slideDown();
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#testitoggle1").text("Read More");
+      $("#testitext1").slideUp();
+    }
+
+});
+
+  $("#testitoggle2").click(function() {
+    var elem = $("#testitoggle2").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#testitoggle2").text("Read Less");
+      $("#testitext2").slideDown();
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#testitoggle2").text("Read More");
+      $("#testitext2").slideUp();
+    }
+
+});
+
+  $("#exp-col1").click(function() {
+    if ($("#ico1").classList.contains("fa-angle-down")) {
+      //Stuff to do when btn is in the read more state
+      $("#ico1").classList.remove("fa-angle-down");
+      $("#ico1").classList.add("fa-angle-up");
+
+      $("#ans1").slideDown();
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#ico1").classList.add("fa-angle-down");
+      $("#ico1").classList.remove("fa-angle-up");
+
+      $("#ans1").slideUp();
+    }
+
+});
+
+})(jQuery); 
+// End of use strict
